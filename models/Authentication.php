2c96756be6d016dafdeb88217fcdc7e99db26f07
Rin Heimerle <rin@Rins-MacBook.local>
2019-03-08 09:29:07 -0400

    Added More comments

--- models/Authentication.php
+++ models/Authentication.php
@@ -10,20 +10,31 @@
             include("../authorized/connection_details.php");
 
             if ($stmt = mysqli_prepare($conn, "SELECT username, password FROM users WHERE username = ?")) {
+                //start the session
                 session_start();
+                //bind params to the stmt
                 $stmt->bind_param('s', $this->username); 
                 $stmt->execute();
+                //store result
                 $stmt->store_result();
-        
+                
+                //bind result
                 $stmt->bind_result($username, $db_password);
+                //fetch the result
                 $stmt->fetch();
 
+                //check the password!!!
                 if (password_verify($this->password, $db_password)) {
+                    //set loggedin to true
                     $_SESSION["loggedin"] = true;
+                    //set session username
                     $_SESSION['username'] = $this->username;
+                    //set session contacts if needed
                     $_SESSION['contacts'] = array();
+                    //redirect
                     header("Location: ../views/authenticated_view.php");exit();
                 } else {
+                    //redirect
                     header("Location: ../index.php");exit();
                 }
             }
--- models/Contact.php
+++ models/Contact.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
     if($_SESSION['loggedin'] == false) header("Location: ../index.php");
+    include("../authorized/connection_details.php");
     class Contact {
         //public variables 
         public $id;
@@ -15,7 +16,6 @@
 
         //add contact function
         public function addContact() {
-            include("../authorized/connection_details.php");
             if($stmt = mysqli_prepare($conn,"INSERT INTO address_book (first_name, surname, phone_number, email_address, postal_code, birthday) VALUES (?,?,?,?,?,?)")) {
                 $stmt->bind_param("ssssss", $this->first_name, $this->surname, $this->phone_number, $this->email_address, $this->postal_code, $this->birthday);
                 $sent = $stmt->execute();
@@ -27,7 +27,6 @@
 
         //edit contact function
         public function editContact() {
-            include("../authorized/connection_details.php");
             if ($stmt = mysqli_prepare($conn, "UPDATE address_book SET first_name=?, surname=?, email_address=?, phone_number=?, postal_code=?, birthday=? WHERE id=?")) {
                 $stmt->bind_param('ssssssi', $this->first_name, $this->surname, $this->email_address, $this->phone_number, $this->postal_code, $this->birthday, $this->id); 
                 $sent = $stmt->execute();
@@ -42,7 +41,7 @@
 
         //delete contact function
         public function deleteContact() {
-            include("../authorized/connection_details.php");
+            //stmt and params
             if($stmt = mysqli_prepare($conn, "DELETE FROM address_book WHERE id=?"));
             $stmt->bind_param('i', $this->id);
             $sent = $stmt->execute();
